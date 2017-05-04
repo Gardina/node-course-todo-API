4fc11599a5fb250aa72daf6ca1826dfa06e513d7
Gardina <andrey.diving@gmail.com>
2017-05-04 15:38:39 +0700

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,20 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => { if (err) {
+	return console.log('unable to connect to mongodb server'); }
+	console.log('Connected to MongoDB server');
+	// db.collection('Todos').find({
+	// 	_id: ObjectID('590ad08cd9ecc136f6629860')
+	// }).toArray().then((docs) => {
+	// 	console.log(JSON.stringify(docs, undefined, 2)); }, (err) => {
+	// 		console.log('unable to fetch todos', err); } );
+	// db.collection('Todos').find().count().then((count) => {
+	// 	console.log(`Todos count: ${count}`); }, (err) => {
+	// 	console.log('unable to fetch todos', err); } );
+
+	db.collection('Users').find({name: 'Andrey'}).toArray().then((docs) => {
+		console.log(JSON.stringify(docs, undefined, 2));
+	});
+	//db.close();
+});
