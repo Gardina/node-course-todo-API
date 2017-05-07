702a4d272036ade930c2e60239252305d5f5c0c6
Gardina <andrey.diving@gmail.com>
2017-05-07 23:23:35 +0700

    heroku6

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds133221.mlab.com:33221/udemytodoapp');
+mongoose.connect('mongodb://admin:admin1232@ds133221.mlab.com:33221/udemytodoapp');
 
 module.exports = {
 	mongoose
