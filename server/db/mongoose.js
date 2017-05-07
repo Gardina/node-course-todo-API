55fc7c87bda76f65f01b3ae420eeb2cc08904e0e
Gardina <andrey.diving@gmail.com>
2017-05-07 22:59:00 +0700

    to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
+var uri = 'mongodb://<dbuser>:<dbpassword>@ds133221.mlab.com:33221/udemytodoapp';
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds133221.mlab.com:33221/udemytodoapp' || 'mongodb://localhost:27017/TodoApp');
+
+mongoose.connect( uri || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
 	mongoose
