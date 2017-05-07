dbdac2e6cbf0e8bb7acd538cbf0a06865f1ae306
Gardina <andrey.diving@gmail.com>
2017-05-07 22:50:29 +0700

    add mongoDB URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds133221.mlab.com:33221/udemytodoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
 	mongoose
