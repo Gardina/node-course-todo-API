a81b8bac1ad11915512a3587053d7af3f028e2a5
Gardina <andrey.diving@gmail.com>
2017-05-09 00:00:39 +0700

    add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect( 'mongodb://admin:admin123@ds133221.mlab.com:33221/udemytodoapp' || 'mongodb://localhost:27017/TodoApp');
-
+mongoose.connect( 'mongodb://localhost:27017/TodoApp' || 'mongodb://admin:admin123@ds133221.mlab.com:33221/udemytodoapp');
 module.exports = {
 	mongoose
 };
