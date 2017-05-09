bba60e65224723731d24311619ed447ab7f3b461
Gardina <andrey.diving@gmail.com>
2017-05-10 05:15:49 +0700

    add PATCH for todo

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,17 +8,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	console.log('Connected to mongoDB server');
 
 
-	// db.collection('Todos').findOneAndUpdate({
-	// 	_id: new ObjectID('590acf784904af36b7d7b5ad')
-	// }, {
-	// 	$set: {
-	// 		completed: true
-	// 	}
-	// }, {
-	// 	returnOriginal: false
-	// }).then((result) => {
-	// 	console.log(result);
-	// });
+	db.collection('Todos').findOneAndUpdate({
+		_id: new ObjectID('590acf784904af36b7d7b5ad')
+	}, {
+		$set: {
+			completed: true
+		}
+	}, {
+		returnOriginal: false
+	}).then((result) => {
+		console.log(result);
+	});
 
 	db.collection('Users').findOneAndUpdate({
 		_id: new ObjectID('590ac17ba117773263d97303')
