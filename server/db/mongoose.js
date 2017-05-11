96d088e757849c8fa9fa98537f67ff921aae98e4
Gardina <andrey.diving@gmail.com>
2017-05-11 21:23:09 +0700

    add process.env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
-//mongoose.Promise = global.Promise;
-mongoose.connect( 'mongodb://admin:admin123@ds133221.mlab.com:33221/udemytodoapp' || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.Promise = global.Promise;
+mongoose.connect( process.env.'mongodb://admin:admin123@ds133221.mlab.com:33221/udemytodoapp' || 'mongodb://localhost:27017/TodoApp');
+
 module.exports = {
 	mongoose
 };
