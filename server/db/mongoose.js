dfcf116d011495afa9ee050a1afab9ff06b4a3c5
Gardina <andrey.diving@gmail.com>
2017-05-11 21:29:20 +0700

    heroku back2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,5 +4,5 @@ mongoose.Promise = global.Promise;
 mongoose.connect( 'mongodb://admin:admin123@ds133221.mlab.com:33221/udemytodoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
-	mongooses
+	mongoose
 };
