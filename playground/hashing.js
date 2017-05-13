ca68ba788c53b5819cf2dfa7a4a6ee723521201b
Gardina <andrey.diving@gmail.com>
2017-05-13 23:33:19 +0700

    add hash for user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-var data = {
-	id: 10
-};
+const bcrypt = require('bcryptjs');
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+var password = '123abc';
+
+bcrypt.genSalt(10, (err,salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
+
+var hashedPassword = '$2a$10$2aBFAc.KBpGlZUC6VpDn8.qShDyTu/n1th3EL.RLP1qAyCe7xm522';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	concole.log(res);
+});
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 // var message = 'I am user 4';
 // var hash = SHA256(message).toString();
 //
